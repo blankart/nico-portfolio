63a653961cef8fa6172a87a45e8833fe209a8462
Nico Mendoza <blankartwp@gmail.com>
2023-07-31 11:52:51 +0800

    feat: added verification meta for backlink.cafe

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html className="dark" lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="verification"
+          content="18d40f767215c97fef1beb6b1641362e28dc"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
